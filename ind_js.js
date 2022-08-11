a4ec56e9b77ecb7c0f9def73f849426979e561f6
minhong1126 <83833271+minhong1126@users.noreply.github.com>
2022-08-11 22:32:21 +0900

    Add files via upload

---
+++ ind_js.js
@@ -0,0 +1,7 @@
+//슬라이드 사진
+
+//슬라이드 화살표
+var sld_prev = document.getElementsByClassName("sld_prev");
+var sld_next = document.getElementsByClassName("sld_next");
+sld_prev.innerHTML = "<";
+sld_next.innerHTML = ">";
