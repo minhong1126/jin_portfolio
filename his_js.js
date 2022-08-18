bfbea284751e05b1e366c9c9d0d112cbf60274ab
minhong1126 <83833271+minhong1126@users.noreply.github.com>
2022-08-18 17:47:49 +0900

    Add files via upload
    2022.8.18.

--- his_js.js
+++ his_js.js
@@ -1,7 +1,6 @@
 //경력
-var rsm = document.getElementById("rsm");
+var rsm = document.querySelectorAll("rsm");
 
-rsm[0].onclick(function(){
-    var string = "아이들 영어 가르치기";
-    rsm.appendChild(string);
-})
+var rsm_clk = function(){
+    
+};
--- ind_js.js
+++ ind_js.js
@@ -1,19 +1,23 @@
+//미완성 알림
+alert("아직 미완성입니다!");
+
 //슬라이드 사진
 var img = document.getElementsByClassName("sld_img");
+var curr_img = img[0];
 
 //슬라이드 화살표
 var sld_prev = document.getElementsByClassName("sld_prev");
 var sld_next = document.getElementsByClassName("sld_next");
 
 var i = 0;
-sld_prev.onclick(function(){
+sld_prev.onclick = function(){
     i = (i - 1) % 3 + 3;
     for(j = 0; j < 3; j++){
         if(j == i){
-            img[j].style.visibilty = "visible";
+            img[j].style.display = "block";
         }
         else{
-            img[j].style.visibilty = "invisible";
+            img[j].style.visibilty = "none";
         }
     }
-})
+};
