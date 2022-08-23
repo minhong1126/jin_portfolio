eb8c5153fac8ce69226cfaeefda8129c83cbc7bc
minhong1126 <83833271+minhong1126@users.noreply.github.com>
2022-08-23 22:04:17 +0900

    Add files via upload
    2022.8.23.

--- his_js.js
+++ his_js.js
@@ -1,6 +1,11 @@
 //경력
-var rsm = document.querySelectorAll("rsm");
+var rsm_btn1 = document.getElementById("rsm_btn1");
+var rsm_btn2 = document.getElementById("rsm_btn2");
+var rsm_btn3 = document.getElementById("rsm_btn3");
 
-var rsm_clk = function(){
-    
-};
+rsm_btn1.addEventListener("click", btn1());
+
+
+function btn1(){
+    alert("히히");
+}
